42c457cd2bcdfa6c325d41a6eb16923f756bf5d7
Md. Shariful Islam <sharif@mediusware.com>
2026-02-18 16:35:41 +0600

    Init project monorepo

---
+++ gRPCServiceB/scripts/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#set -Eero pipe fail
+# shellcheck disable=SC2164
+
+cd /var/www/html
+composer install --no-interaction --optimize-autoloader
+
+chmod -R 777 storage/
+chmod -R 775 .env
+chmod -R 775 bootstrap/cache
+chmod -R 775 public
+chown -R www-data:www-data public/
+
+php artisan storage:link
+php artisan migrate --force
+php artisan o:c
+
+service php8.4-fpm restart
+service supervisor start
+supervisorctl reread
+supervisorctl update
+supervisorctl start all
+
+exec nginx -g "daemon off;"
